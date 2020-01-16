e5ad80118a0c86bce4f881fec5d44a000d51a74d
bafxmartin <bafxmartin@verizon.net>
2020-01-15 22:18:06 -0500

    Add java doc; add index endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -6,10 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PlantPlacesController {
 
+	/**
+	 * Handle start point
+	 * @return
+	 */
 	@RequestMapping("/start")
 	public String start() {
 		return "start";
 	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "start";
+	}
+	
 	
 }
 
