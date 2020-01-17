d9f0f0a8694543c309202d93e89e274e88ea2e9b
bafxmartin <bafxmartin@verizon.net>
2020-01-16 21:37:44 -0500

    Postman Get, Post, Put, Delete, Patch

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,20 +1,30 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PlantPlacesController {
 
 	/**
-	 * Handle start point
+	 * Handle the /start point
 	 * @return
 	 */
-	@RequestMapping("/start")
-	public String start() {
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read() {
 		return "start";
 	}
 	
+	@PostMapping("/start")
+	public String create() {
+		return "start";
+	}
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 */
 	@RequestMapping("/")
 	public String index() {
 		return "start";
