3b467c335cf309a187c1ad5207c6faa1098959b8
bafxmartin <bafxmartin@verizon.net>
2020-01-19 20:35:23 -0500

    value="latitude", required=false

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.plantplaces.dto.SpecimenDTO;
@@ -48,6 +49,15 @@ public class PlantPlacesController {
 	public String readJSON() {
 		return "start";
 	}
+	
+	@RequestMapping(value="/addspecimen", method=RequestMethod.GET)
+	public String addspecimen(Model model, @RequestParam(value="latitude", required=false, defaultValue="0.0") String latitude) {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		specimenDTO.setLatitude(latitude);
+		model.addAttribute("specimenDTO", specimenDTO);
+		return "start";
+	}
+	
 	@PostMapping("/start")
 	public String create() {
 		return "start";
